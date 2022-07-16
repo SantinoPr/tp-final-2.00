7c3d50d830f31e588bdd1f0149dc15368991e2f2
Santino <Santino@DESKTOP-S1MFQC6>
2022-07-16 19:24:17 -0300

    Agregar archivos de proyecto.

---
+++ Equipo.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp_final_2
+{
+    class Equipo
+    {
+        int faltas;
+        int cont = 0;
+        Jugador[] jugador = new Jugador[5];
+        string nombreEquipo;
+        
+      
+        public void CrearJ(string nombre, int num)
+        {
+            jugador[cont] = new Jugador(num, nombre);//se crean los jugadores
+            cont++;
+        }
+
+
+        public void NombreEquipo(string nombre)//se ingresa el nombre del equipo
+        {
+            nombreEquipo = nombre;
+
+        }
+        public int PuntosJuga(int indice)//puntos de cada jugador
+        {
+            return jugador[indice].Puntos;
+        }
+        public string NombreE//retorna el nombre del equipo
+        {
+            get { return nombreEquipo; }
+        }
+
+
+        public void Anotar(int tanto, int indice)//anotacion de los puntos
+        {
+            jugador[indice].Marcador(tanto);
+
+        }
+        public void burbuja()//metodo de ordenamiento
+        {
+            Jugador aux = new Jugador(0, "cero");
+          
+            bool bandera = false;
+
+            for (int i = 0; i < jugador.Length - 1; i++)
+            {
+                if (bandera)
+                {
+                    break;
+                }
+                bandera = true;
+                for (int j = 0; j < jugador.Length - 1; j++)
+                {
+                    if (jugador[j].Puntos > jugador[j + 1].Puntos)
+                    {
+                        
+                        aux = jugador[j];
+                        jugador[j] = jugador[j+1];
+                        jugador[j+1] = aux;
+                        bandera = false;
+
+                    }
+                }
+            }
+        }
+        public string Nom(int indice)//retorna solo el nombre del jugador
+        {
+            return jugador[indice].Nombre;
+
+        }
+
+
+        public int PuntosTotales()//puntos totales realizados por el equipo
+        {
+            int acum = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                acum += jugador[i].Puntos;
+
+            }
+            return acum;
+        }
+
+
+        public void Faltas(int indice)//faltas cometidas por el jugador guardado en el indice
+        {
+            jugador[indice].Falta();
+            faltas++;
+
+        }
+
+        public int FaltasTotales()//faltas totales del equipo
+        {
+            int acum = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                acum += jugador[i].faltastotales;
+
+            }
+
+            return acum;
+        }
+
+        public bool PuedeJugar(int indice)
+        {
+            return jugador[indice].PuedeJugar();
+
+        }
+
+        public string Mayores()//jugador que mas tantos hizo
+        {
+            string Mayornombre = "";
+            int Mayorpuntos = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (jugador[i].Puntos > Mayorpuntos)
+                {
+                    Mayorpuntos = jugador[i].Puntos;
+                    Mayornombre = jugador[i].Nombre;
+                }
+
+            }
+
+            return String.Format("{0} - {1} en total", Mayornombre, Mayorpuntos);
+
+
+        }
+
+        public string Menores()//jugador que menos tanto realizo
+        {
+            string Menornombre = "";
+            int Menorpuntos=PuntosTotales();
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (jugador[i].Puntos < Menorpuntos)
+                {
+                    Menorpuntos = jugador[i].Puntos;
+                    Menornombre = jugador[i].Nombre;
+                }
+
+            }
+
+            return String.Format("{0} - {1} en total", Menornombre, Menorpuntos);
+
+
+        }
+    }
+}
---
+++ Jugador.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp_final_2
+{
+    class Jugador
+    {
+        int nroJugador;
+        string nombre;
+        int faltas;
+        int tanto;
+        
+
+         public Jugador(int numero, string nombre)//contructor del jugador
+         {
+            nroJugador= numero;
+            this.nombre = nombre;
+         }
+        
+        public int NroCamiseta
+        {
+            get { return nroJugador; }
+        }
+        public string Nombre
+        {
+            get { return nombre; } 
+        }
+
+        public void Marcador(int puntos) //puntos totales
+        {
+            tanto += puntos;
+        }
+
+        
+
+        public int Puntos//retorna los puntos del jugador
+        {
+            get { return tanto; } set { tanto = value; } 
+
+        }
+
+        public int faltastotales //faltas totales del jugador
+        {
+            get { return faltas; }
+        }
+        
+        public bool PuedeJugar()//comprueba si tiene menos de 5 faltas
+        {
+            bool Faltas = false;
+
+            if(faltas >= 5)
+            {
+                 Faltas= true;
+            }
+            return Faltas;
+
+        }
+
+        
+
+        public void Falta()
+        {
+            faltas++;
+
+        }
+
+        }
+    }
+
+
